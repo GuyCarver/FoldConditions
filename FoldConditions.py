82693570c7d48733fad108f402d188ba55977c50
GuyCarver <gcarver@bethsoft.com>
2015-03-28 18:32:55 -0400

    Improvements

--- FoldConditions.py
+++ FoldConditions.py
@@ -10,6 +10,7 @@ MaxStack = 100
 def Defined( aWord, TestDigit = True ) :
   '''Determine if the given word is in our define list.  If it's a non-0
     numbers return true.'''
+#  dbgprint(aWord)
   if aWord.isdigit() :
     #TestDigit false means we don't consider digits so we always return True.
     return True if not TestDigit else (aWord != "0")
@@ -71,19 +72,33 @@ moredefA = [
 
 #List of #if expressions.
 ifdefA = [
-(True, re.compile("#[ \t]*if(?P<df>def)?[ \t]*(?(df)|(defined))[ \t]*\(?(\w+)\)?(.*)")),
-(False, re.compile("#[ \t]*if(?P<df>ndef)?[ \t]*(?(df)|(!defined))[ \t]*\(?(\w+)\)?(.*)")),
-(True, re.compile("#[ \t]*if[ \t]*([\(\)\w]+)"))
+(True, re.compile("^[ \t]?#[ \t]*if(?P<df>def)?[ \t]*(?(df)|(defined))[ \t]*\(? ?(\w+)\)?(.*)")),
+(False, re.compile("^[ \t]?#[ \t]*if(?P<df>ndef)?[ \t]*(?(df)|(!defined))[ \t]*\(? ?(\w+)\)?(.*)")),
+(True, re.compile("^[ \t]?#[ \t]*(if)[ \t]*(([\(\)\w]+))"))
 ]
 
 #list of elif expressions
 elifA = [
-(True, re.compile("#[ \t]*elif[ \t]*defined\((.*)\)")),
-(False, re.compile("#[ \t]*elif[ \t]*!defined\((.*)\)")),
+(True, re.compile("^[ \t]?#[ \t]*elif[ \t]*defined\((.*)\)")),
+(False, re.compile("^[ \t]?#[ \t]*elif[ \t]*!defined\((.*)\)")),
 ]
 
-elsesearch = re.compile("#[ \t]*else.*")
-endifsearch = re.compile("#[ \t]*endif.*")
+elsesearch = re.compile("^[ \t]?#[ \t]*else.*")
+endifsearch = re.compile("^[ \t]?#[ \t]*endif.*")
+
+FoldRegKey = "FoldRegion"
+
+class FoldSetRegionCommand( sublime_plugin.TextCommand ) :
+  '''Set region for fold to work within.
+      If selection empty the region will be cleared.'''
+
+  def run( self, edit ) :
+    vw = self.view
+    s = vw.sel()[0]
+    if s.empty() :
+      vw.erase_regions(FoldRegKey)
+    else:
+      vw.add_regions(FoldRegKey, [s], scope = "blue", flags = sublime.DRAW_NO_FILL)
 
 class DefineCommand( sublime_plugin.TextCommand ) :
   '''Grab word under selections do the desired action on the def list.'''
@@ -137,14 +152,15 @@ def CheckMore( aValue, aLine ) :
 
   return aValue
 
-NodeStack = None #Stack of state, Range, Eval, Sibling, Child
+nsState, nsRange, nsEval, nsSibling, nsChildren = range(5)
+NodeStack = None #Stack of (state, Range, Eval, Sibling, Children)
 view = None
 
 def PushChild( aState, aRange, aEval ) :
   '''Push data onto the child list of the top state in the stack'''
   global NodeStack
   node = (aState, aRange, aEval, [], [])
-  NodeStack[0][4].insert(0, node)
+  NodeStack[0][nsChildren].insert(0, node)
   NodeStack.insert(0, node)
 
 def Pop( ) :
@@ -159,7 +175,7 @@ def PushSibling( aState, aRange, aEval, aPush = True ) :
   '''Push data onto the sibling list of the top state in the stack'''
   global NodeStack
   node = (aState, aRange, aEval, [], [])
-  NodeStack[0][3].insert(0, node)
+  NodeStack[0][nsSibling].insert(0, node)
   res = Pop()
   if (aPush):
     NodeStack.insert(0, node)
@@ -173,6 +189,8 @@ def EvalFree( aRange, aHidden ) :
 def EvalIf( OnOff, aRes, aRange, aHidden ) :
   '''Evaluation function for an if expression.'''
 #  dbgprint(aRes.groups())
+#  row, _ = view.rowcol(aRange.a)
+#  dbgprint(row)
   defd = OnOff if Defined(aRes.group(3)) else not OnOff
 #  dbgprint("{} last index {}".format(res.group(1), res.lastindex))
   if aRes.lastindex == 4 :
@@ -209,6 +227,7 @@ def IfDef( aRange, aLine ) :
   for srch in ifdefA :
     res = srch[1].search(aLine)
     if res and res.lastindex :
+#      dbgprint("Define:", aLine)
       PushChild(ifstate, aRange, functools.partial(EvalIf, srch[0], res))
       return True
   return False
@@ -232,6 +251,7 @@ def EndIf( aRange, aLine ) :
   '''Determine if line is an #endif expression.'''
   res = endifsearch.search(aLine)
   if res != None :
+#    dbgprint("Endif:", aLine)
     return PushSibling(freestate, aRange, EvalEndIf, False)
   return False
 
@@ -259,6 +279,15 @@ def elsestate( aRange, aLine ) :
             return "Nested Else"
   return None
 
+def PatchEndifs( aView, aDest ) :
+  '''Some #endifs are not correctly tagged so we have to search for
+      them manually and add them to the list.'''
+  ees = aView.find_all(endifsearch.pattern)
+  for ee in ees:
+    score = aView.score_selector(ee.begin(), 'meta.preprocessor.c')
+    if not score :
+      aDest.append(ee)
+
 def FillNodeStack( aView ) :
   global NodeStack
   global view
@@ -266,26 +295,39 @@ def FillNodeStack( aView ) :
   view = aView
 
   #Start in a free state with no region.
-  NodeStack = [(freestate, None, EvalFree, [], [])] #Stack of state, Region, Eval, Sibling, Child
+  NodeStack = [(freestate, None, EvalFree, [], [])] #Stack of state, Region, Eval, Sibling, Children
 
   #Find #ifdef, #endif.
   conditionRegs = aView.find_by_selector("preprocessor.keyword.control.import.c")
-  #Fend #else, #elif
+  #Find #else, #elif
   conditionRegs = conditionRegs + aView.find_by_selector("preprocessor.import.control.keyword.c")
+  #Find #if
+  conditionRegs = conditionRegs + aView.find_by_selector("preprocessor.keyword.control.import.if.c")
+  #Find #else matched with #if.
+  conditionRegs = conditionRegs + aView.find_by_selector("preprocessor.keyword.control.import.else.c")
+  #Make sure no untagged endifs are left out.
+  PatchEndifs(aView, conditionRegs)
+
   #Sort all the entries by location.
   conditionRegs = sorted(conditionRegs, key = lambda r: r.begin())
+
+  ss = aView.get_regions(FoldRegKey)
+  if len(ss) :
+    s = ss[0]
+    if not s.empty() :
+      conditionRegs = [ x for x in conditionRegs if x.intersects(s) ]
 
   for r in conditionRegs:
     ln = aView.line(r)
     line = aView.substr(ln)
-#      dbgprint(aView.rowcol(ln.begin()), line)
+#    dbgprint(aView.rowcol(ln.begin()), line)
     #Run the line through the top state on the stack.
-    err = NodeStack[0][0](ln, line)
+    err = NodeStack[0][nsState](ln, line)
     #if err is set then we ran into an error so show it to the user.
     if err :
       row, _ = aView.rowcol(ln.begin())
       msg = "{} line {} - {}".format(err, row + 1, line)
-#        dbgprint(msg)
+      print(msg)
       aView.show_at_center(ln)
       aView.sel().clear()
       aView.sel().add(ln)
@@ -294,7 +336,7 @@ def FillNodeStack( aView ) :
 
   if len(NodeStack) > 1 :
     for s in NodeStack[:-1] :
-      ln = aView.line(s[1])
+      ln = aView.line(s[nsRange])
       row, _ = aView.rowcol(ln.begin())
       line = aView.substr(ln)
       print("Unclosed condition", row + 1, line)
@@ -325,25 +367,25 @@ class FoldConditionsCommand( sublime_plugin.TextCommand ) :
     '''Recursively run through the state trees and process the regions.'''
 
     #run the state evaluation function with the region and current Hidden state.
-    aHidden = aState[2](aState[1], aHidden)
+    aHidden = aState[nsEval](aState[nsRange], aHidden)
 #    dbgprint(aHidden)
 
     #if the state is active then run the children
     if not aHidden :
       #If folding stop it as we are now in an open state.
       if self.Folding :
-        self.StopFolding(aState[1])
+        self.StopFolding(aState[nsRange])
 
       #process the children because they will be visible.
-      for child in aState[4]:
+      for child in aState[nsChildren]:
         self.crawl(child, aHidden)
     else:
       #if not folding start folding.
       if not self.Folding:
-        self.StartFolding(aState[1])
+        self.StartFolding(aState[nsRange])
 
     #Process the siblings.
-    for sibling in aState[3]:
+    for sibling in aState[nsSibling]:
       aHidden = self.crawl(sibling, aHidden)
 
   def reset( self ) :
@@ -375,23 +417,59 @@ class MatchingConditionCommand( sublime_plugin.TextCommand ) :
     global view
     NodeStack = None
     view = None
+    self.MyLine = None
+
+  def findother( self, aNodes ) :
+    for node in aNodes :
+      while node :
+        prevNode = node
+        node = prevNode[nsSibling][0] if len(prevNode[nsSibling]) else None
+        if node :
+#          ln = self.view.line(prevNode[1])
+#          line = self.view.substr(ln)
+#          ln2 = self.view.line(node[1])
+#          line2 = self.view.substr(ln2)
+#          dbgprint("looking", line, line2)
+
+          if prevNode[nsRange].a == self.MyLine.a :
+            return node
+          elif node[nsRange].a == self.MyLine.a :
+            return prevNode
+
+          r = prevNode[nsRange].cover(node[nsRange])
+          if r.contains(self.MyLine.a) :
+            return self.findother(prevNode[nsChildren])
+
+    return None
 
   def run( self, edit ) :
     vw = self.view
 
     #todo: Check current line for correct type.
-    ln = vw.line(vw.sel()[0])
-    c1 = "preprocessor.keyword.control.import.c"
+    s = vw.sel()[0]
+    ln = vw.line(s)
+    #note: This will match pragmas as well but I don't worry about that.
+    c1 = "preprocessor.keyword.control.import.if.c"
     c2 = "preprocessor.import.control.keyword.c"
     score = vw.score_selector(ln.begin(), c1)
     score |= vw.score_selector(ln.begin(), c2)
+    #If beginning of line doesn't match score try cursor position.
+    if not score :
+      score = vw.score_selector(s.a, c1)
+      score |= vw.score_selector(s.a, c2)
+
 #    line = vw.substr(ln)
 #    dbgprint(line, "is", score)
 
     if score :
-      if FillNodeStack(self.vw) :
-        #todo: run
-        pass
+      if FillNodeStack(vw) :
+        self.MyLine = ln
+        res = self.findother(NodeStack[0][nsChildren])
+        if res :
+          p = res[nsRange].a
+          vw.show_at_center(p)
+          vw.sel().clear()
+          vw.sel().add(sublime.Region(p, p))
 
       self.reset()
 
